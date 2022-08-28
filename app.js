53ac05228142cc1cf1464a14ec9826cdb6829264
Samir Gajanan Katurde <sam-mac@Samirs-MacBook-Air.local>
2022-08-28 17:20:33 +0530

    changes done

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express();
+
+app.get('/', (req, res, next) =>{
+	res.send(" This is Main Page !")
+})
+
+const port = "3000";
+app.listen( port, ()=>{
+  console.log(`Node Js Server is running on the port: ${port}`);  
+});
+
+
